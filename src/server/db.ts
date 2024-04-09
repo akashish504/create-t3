506d9fe16b02378c0c9263f5fccf18676e0bb68d
Ashish <akashish504@gmail.com>
2024-04-09 15:56:13 +0530

    removing redundant code

--- src/server/db.ts
+++ src/server/db.ts
@@ -15,21 +15,3 @@ const globalForPrisma = globalThis as unknown as {
 export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
-
-import { faker } from '@faker-js/faker';
-
-function func() {
-console.log(faker.commerce.department())
-// faker.commerce.department() // 'Garden'
-
-
-
-for (let i = 0; i < 100; i++) {
-  db.product.create({
-    name: faker.commerce.department(),
-  });
-}
-}
-
-func();
